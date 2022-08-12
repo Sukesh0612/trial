8e0c9b7de7680423b9b08aa31a4b512c50ee815f
Sukesh0612 <ussukesh@gmail.com>
2022-08-12 15:21:15 +0530

    commit-1

---
+++ trial1/src/main/java/objectRepository/PaymentPage.java
@@ -0,0 +1,36 @@
+package objectRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PaymentPage {
+
+	//declaration
+
+	//==============================Declaration of Elements================================	
+
+
+	@FindBy(name = "submit")
+	private WebElement submitBtn;
+
+	//==============================================================================================	
+
+	//initialization
+
+	public PaymentPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+
+	//Utilization
+
+	//===============================Utilization for Sign In======================================
+
+	public WebElement getSubmitBtn()
+	{
+		return submitBtn;
+	}
+
+}
